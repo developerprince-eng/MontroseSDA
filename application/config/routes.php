b05f6e252683f9135be6670ad290319e549e2741
Princr <prince@techvillage.org.zw>
2019-08-26 16:16:52 +0200

    adjustments

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,9 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$route['default_controller'] = 'main';
-$route['(:any)'] = 'main/$1';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
 $route['events'] = 'events'; 
 $route['sermons'] = 'sermons';
 $route['church-building'] = 'main/buildingproject';
@@ -11,3 +7,6 @@ $route['contact'] = 'main/contact';
 $route['ministries'] = 'ministries';
 $route['news'] = 'news';
 $route['gallery'] = 'gallery';   
+$route['default_controller'] = 'main';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
