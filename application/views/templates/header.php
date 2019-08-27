29fc6349baa2a45ef41d16e6763e8900c4874796
Princr <prince@techvillage.org.zw>
2019-08-27 08:30:02 +0200

    structure data attachment

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,5 +8,52 @@
     <meta name="author" content="">
     <!-- LOAD CSS FILES -->
     <link href="<?php echo base_url(); ?>static/css/main.css" rel="stylesheet" type="text/css">
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org/",
+            "@type": "Website",
+            "url": "http://www.montrosesda.org.zw/",
+            "name": "MontroseSDA Church WebSite",
+            "author": {
+                "@type": "Person",
+                "name": "Prince Kudzai Maposa",
+                "familyName": "Maposa",
+                "givenName": "Prince",
+                "gender": "Male",
+                "JobTitle": "Full Stack Web Developer & Systems Developer",
+                "url": ["https://twitter.com/kudzai_prince", "twiphoto.com/kudzai_prince"]
+            },
+            "publisher": {
+                "@type": "Person",
+                "name": "Prnce Kudzai Maposa",
+                "familyName": "Maposa",
+                "givenName": "Prince",
+                "email": "prince@developerprince.co.zw",
+                "gender": "Male",
+                "JobTitle": "Full Stack Web Developer & Systems Developer"
+            },
+            "description": "Montrose SDA Church is small church which belongs to the city center District",
+            "potentialAction": {
+                "@type": "SearchAction",
+                "target": "http://www.montrosesda.org.zw/?s={montrosesda}",
+                "query-input": "required name=montrosesda"
+            }
+        }
+        "nationality": {
+            "@type": "Country",
+            "name": "Zimbabwe"
+        },
+        "affiliation": [{
+            "@type": "Organization",
+            "name": "The DevShop",
+            "url": "http://devshop.co.zw",
+            "sameAs": ["https://facebook.com/TDevshop", "https://twitter.com/devshopzw"]
+        }, {
+            "@type": "Organization",
+            "name": "The Tech Village",
+            "url": "http://techvillage.org.zw"
+        }]
+        }
+    </script>
 </head>
 <body>
